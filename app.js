3e83b0ef22796b2a5bd5a674881f07ec3abab0b7
K-Mo2 <karimelsisi27@gmail.com>
2021-11-09 11:07:30 +0200

    practicing npm packages and testing email and url validity with 'validator' package

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const getNotes = require("./notes");
-
+// const getNotes = require("./notes");
+const validator = require("validator");
 // const fs = require("fs");
 // fs.writeFileSync("notes.txt", "This is writen by node\n");
 
 // fs.appendFileSync("notes.txt", "This is line is appended by node");
 
- getNotes("these are my notes");
+//  getNotes("these are my notes");
+
+console.log(validator.isEmail("example@example.com"));
+console.log(validator.isURL("www.exampleexample.com"));
+
+console.log(validator.isEmail("exampleexample.com"));
+console.log(validator.isURL("wwwexampleexample"));
