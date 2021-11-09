726fb543b8dcbad584b86b6dc5bc8bc0eafd3f61
K-Mo2 <karimelsisi27@gmail.com>
2021-11-09 12:40:25 +0200

    Argument parsing with Yargs

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const yargs = require("yargs");
 
-console.log(process.argv);
-
-yargs.version("1.1.0");
-
 yargs.command({
   command: "add",
   description: "adding a new note",
-  handler: function () {
-    console.log("Adding a new note");
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title:" + argv.title, "\nBody:" + argv.body);
   },
 });
 
