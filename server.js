dffa1d86a483057e8e54b8e0b73226db89166ac5
K-Mo2 <karimelsisi27@gmail.com>
2021-11-12 10:54:15 +0200

    adding query check

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-app.get("/home", (req, res) => {
-  res.send("Hello World !");
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "Address must be provided",
+    });
+  }
+
+  res.send({
+    address: "street,city,country",
+  });
 });
 
 app.listen(port, () => {
